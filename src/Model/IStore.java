d1104c9521448f2f2ba536c18c3406d431b8155c
Paez11 <victorpaez117@gmail.com>
2022-01-25 13:00:42 +0100

    Interface create

---
+++ src/Model/IStore.java
@@ -0,0 +1,16 @@
+package Model;
+
+public interface IStore {
+	boolean addDrink(IDrink drink);
+	boolean searchDrink(String name);
+	IDrink getDrink(String name);
+	boolean updateDrink(String name, IDrink drink);
+	boolean isFull();
+	Float howMuch();
+	/**
+	 * 
+	 * @param type of drink to be counted
+	 * @return
+	 */
+	Integer howMany(DrinkType type);
+}
